186cd63490cbb16f3957ca6fa09400e9eeef42fe
MohammadAlHabil <mohammadalhabil1999@gmail.com>
2022-03-15 19:37:05 +0200

    add env config

--- server/app.js
+++ server/app.js
@@ -1,19 +1,18 @@
-
 const { join } = require('path');
 const express = require('express');
 const compression = require('compression');
-const router = require('./router');
-
+const userRouter = require('./router/user');
 
 const app = express();
 
 app.disable('x-powered-by');
 app.use(compression());
-
 app.use(express.json());
-app.use(express.urlencoded({extended: false}));
-app.use(express.static(join(__dirname, '..' , 'public')));
+require('dotenv').config();
+
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(join(__dirname, '..', 'public')));
 app.set('port', process.env.PORT || 8000);
-app.use(router)
+app.use('/', userRouter);
 
 module.exports = app;
